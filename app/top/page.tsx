2bce268cf7f6c5d9d40d6f1a139941411544ec3c
“ngitky” <nagi.nagai.nnn@gmail.com>
2024-09-26 22:31:34 +0900

    [Add] Top Page

--- app/top/page.tsx
+++ app/top/page.tsx
@@ -5,7 +5,16 @@ import SphereCanvas from '/Users/manakanon/Work/portfolio/frontend/src/component
 export default function TopPage() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <SphereCanvas />
+      <SphereCanvas 
+      elementType="Dot"  // 'Dot' または 'Hexagon' を指定
+      elementQuantity={100}  // 数字で要素の数を指定
+      areElementsRandomized={true}  // ランダム化をオンにするかどうか
+      primaryDelay={500}  // 遅延をミリ秒単位で指定
+      cameraPosition={[0, 0, 5]}  // カメラの位置を指定
+      cameraFOV={75}  // カメラの視野角 (Field of View)
+      lightIntensity={1.5}  // ライトの強さ
+      lightPosition={[10, 10, 10]}  // ライトの位置
+      />
     </div>
   );
 }
